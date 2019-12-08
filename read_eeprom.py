c81f75ba3d8b876ec79191177c5be15bd41b9f99
Raphael Coeffic <raphael.coeffic@frafos.com>
2019-12-08 11:45:19 +0100

    improved python utils

--- read_eeprom.py
+++ read_eeprom.py
@@ -1,14 +1,28 @@
+#!/usr/bin/env python
+
 import serial
 import sys
 import time
-import binascii
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 
 ser = serial.Serial(sys.argv[1], timeout=0.2)
+outf = open(sys.argv[2], 'wb')
 
 ser.write(b'R\n') # EEPROM Read command
 
 # Read 64KB
 for i in range(0, 1024):
-    sys.stdout.buffer.write(ser.read(64))
+    outf.write(ser.read(64))
+    printProgressBar(i, 1023, suffix='downloaded', length=50)
 
 ser.close()
--- write_eeprom.py
+++ write_eeprom.py
@@ -1,25 +1,43 @@
+#!/usr/bin/env python
+
 import serial
 import sys
 import time
 import binascii
 
-ser = serial.Serial(sys.argv[1], baudrate=115200, timeout=0.50)
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
-ser.write(b'W\n') # EEPROM Read command
+ser = serial.Serial(sys.argv[1], baudrate=115200, timeout=0.5)
+inf = open(sys.argv[2], 'rb')
 
-for i in range(0, 1024): # 32 * 1024 = 32KB
-    ser.write(b'0123456789ABCDEF                ')
-    while ser.out_waiting:
-        pass
+ser.write(b'W\n') # EEPROM Write command
+
+i = 0
+while True:
+
+    b = inf.read(32)
+    if len(b) == 0:
+        break
+    
+    ser.write(b)
+    ser.flush()
+
     if i % 2 == 1:
-        print(ser.readline())
+        line = ser.readline().decode('ascii')
+        if line[0] == '#':
+            print('\nErrory detected!')
+            break
+    i = i + 1
 
-for i in range(0, 1024): # 32 * 1024 = 32KB
-    ser.write(b'0123456789ABCDEF ooOO00OOoo     ')
-    while ser.out_waiting:
-        pass
-    if i % 2 == 1:
-        print(ser.readline())
+    printProgressBar(i, 2048, suffix='uploaded', length=50)
 
-print(ser.readline())
+print(ser.readline().decode('ascii'))
 ser.close()
